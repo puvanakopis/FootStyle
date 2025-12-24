ed1032e6099af67fd118b777211c65c34143413e
puvanakopis <puvanakopis@gmail.com>
2025-12-24 15:44:35 +0530

    added the Profile breadcrumbItems

--- frontend/app/account/page.tsx
+++ frontend/app/account/page.tsx
@@ -1,13 +1,19 @@
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Breadcrumbs from '@/components/Breadcrumbs';
 import Sidebar from '@/containers/account/Sidebar';
 
+const breadcrumbItems = [
+    { label: "Account", href: "/account" },
+    { label: "Profile", href: "/account/profile" },
+];
 
 export default function Account() {
     return (
         <main className="min-h-screen bg-background text-foreground">
             <Header />
             <div className="px-30 py-6">
+                <Breadcrumbs items={breadcrumbItems} />
                 <div className="flex flex-col lg:flex-row gap-6 mt-6">
                     {/* Sidebar with fixed width */}
                     <div className='w-full lg:w-1/5'>
