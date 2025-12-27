18ae8e983f48aba88f95b70a59fcba5ef54b4a03
puvanakopis <puvanakopis@gmail.com>
2025-12-27 12:14:39 +0530

    created Payment page with breadcrumbItems

--- frontend/app/payment/page.tsx
+++ frontend/app/payment/page.tsx
@@ -2,25 +2,23 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import Breadcrumbs from '@/components/Breadcrumbs';
 import PageHeader from '@/components/PageHeader';
-import SavedAddresses from "@/containers/delivery-address/SavedAddresses";
-import YourOrder from "@/containers/delivery-address/YourOrder";
 
 const breadcrumbItems = [
     { label: "Home", href: "/" },
     { label: "Cart", href: "/cart" },
     { label: "Delivery Address", href: "/delivery-address" },
+    { label: "Payment", href: "/payment" },
 ];
 
-export default function DeliveryAddress() {
+export default function Payment() {
     return (
         <main className="min-h-screen bg-background text-foreground">
             <Header />
             <div className="px-30 py-6">
                 <Breadcrumbs items={breadcrumbItems} />
-                <PageHeader title="Shipping Information" />
+                <PageHeader title="Payment Information" />
                 <div className="grid grid-cols-1 lg:grid-cols-12 gap-10 items-start">
-                    <SavedAddresses />
-                    <YourOrder />
+                    Payment Information
                 </div>
             </div>
             <Footer />
