c1d05205cf19152b689c3b684c47d8bf0e9f5c8a
puvanakopis <puvanakopis@gmail.com>
2026-01-13 19:12:46 +0530

    Create separate file for MongoDB connection

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,21 @@
 const express = require("express");
-const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
 const authRoutes = require("./routes/authRoutes");
-const googleAuthRoutes = require('./routes/googleAuthRoutes');
+const googleAuthRoutes = require("./routes/googleAuthRoutes");
 
 dotenv.config();
 
 const app = express();
 
+// Connect to database
+connectDB();
+
 // Middleware
 app.use(express.json({
   strict: true,
-  verify: (req, res, buf, encoding) => {
+  verify: (req, res, buf) => {
     try {
       JSON.parse(buf);
     } catch (e) {
@@ -29,7 +33,7 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use('/api/authGoogle', googleAuthRoutes);
+app.use("/api/authGoogle", googleAuthRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World from Express!");
@@ -40,22 +44,12 @@ app.use((err, req, res, next) => {
   console.error("Global Error Handler:", err.message);
   res.status(400).json({
     message: "Something went wrong!",
-    error: err.message
+    error: err.message,
   });
 });
 
-// Connect to MongoDB and start server
+// Start server
 const PORT = process.env.PORT || 4000;
-
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
-    console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-    process.exit(1);
-  });
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
