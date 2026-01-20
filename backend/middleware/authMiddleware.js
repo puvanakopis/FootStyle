d064710c28047ff27c10971d60c7097cdc317b3e
puvanakopis <puvanakopis@gmail.com>
2026-01-21 00:44:49 +0530

    Implement protect and authorize middleware

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -32,3 +32,17 @@ exports.protect = async (req, res, next) => {
         return res.status(401).json({ message: 'Not authorized, invalid token' });
     }
 };
+
+exports.authorize = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            return res.status(401).json({ message: 'Not authorized' });
+        }
+
+        if (!roles.includes(req.user.role)) {
+            return res.status(403).json({ message: 'Forbidden: You do not have access to this resource' });
+        }
+
+        next();
+    };
+};
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { createProduct } = require("../controller/productController");
+const { authorize, protect } = require("../middleware/authMiddleware");
 
-router.post("/", createProduct);
+router.post("/", protect, authorize('admin'), createProduct);
 
 module.exports = router;
